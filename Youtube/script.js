bab827f9e7b0ff15f2d734ae310c9a14fd183a2a
Farzeer <77016185+Farzeer@users.noreply.github.com>
2025-08-31 09:00:30 +0300

    Update script.js

--- Youtube/script.js
+++ Youtube/script.js
@@ -177,6 +177,27 @@ async function loadPlaylist(playlistInput, apiKey, force = false) {
   } catch (err) {
     statusEl.innerText = `Error fetching playlist: ${err.message}`;
   }
+
+  player.loadVideoById(videoIds[currentIndex]);
+  showGIFs(); // Display the GIFs
+}
+
+function showGIFs() {
+  const gifContainer = document.getElementById('gif-container');
+  gifContainer.innerHTML = `
+    <div class="tenor-gif-embed" data-postid="18110512" data-share-method="host" data-aspect-ratio="1.77778" data-width="100%">
+      <a href="https://tenor.com/view/cat-jam-gif-18110512">Cat Jam GIF</a> from <a href="https://tenor.com/search/cat-gifs">Cat GIFs</a>
+    </div>
+    <div class="tenor-gif-embed" data-postid="16989864924126703156" data-share-method="host" data-aspect-ratio="0.797189" data-width="100%">
+      <a href="https://tenor.com/view/catjam-cat-disco-catjamming-gif-16989864924126703156">Catjam Disco GIF</a> from <a href="https://tenor.com/search/catjam-gifs">Catjam GIFs</a>
+    </div>
+  `;
+
+  // Load Tenor script to render embeds
+  const tenorScript = document.createElement('script');
+  tenorScript.src = "https://tenor.com/embed.js";
+  tenorScript.async = true;
+  document.body.appendChild(tenorScript);
 }
 
 // Event listeners
