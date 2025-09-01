c9451009b1dca58dda5212d024e2fd9157d4af2b
Farzeer <77016185+Farzeer@users.noreply.github.com>
2025-09-01 18:03:10 +0300

    Update script.js

--- Youtube/script.js
+++ Youtube/script.js
@@ -244,7 +244,7 @@ document.getElementById('refreshPlaylist').addEventListener('click', () => {
 
 document.getElementById('nextVideo').addEventListener('click', playNext);
 document.getElementById('prevVideo').addEventListener('click', playPrev);
-
+/*
 window.addEventListener('keydown', (e) => {
   if (e.code === 'ArrowRight') {
     playNext();
@@ -266,7 +266,7 @@ window.addEventListener('keydown', (e) => {
     navigator.mediaSession.setActionHandler('nexttrack', () => playNext());
   }
 });
-
+*/
 document.getElementById('cachedPlaylists').addEventListener('change', (e) => {
   const apiKey = document.getElementById('apiKey').value.trim();
   const playlistId = e.target.value;
@@ -283,6 +283,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   updateDropdown();
 });
+
 
 
 
